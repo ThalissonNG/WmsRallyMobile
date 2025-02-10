57a72c884dda7c2f030565019609621cfc7f53f2
ThalissonM <manoelthalisson28@gmail.com>
2025-02-10 10:40:50 -0300

    Update separarPedido.py
    Colocado campo para buscar o pedido

--- routes/separarPedido.py
+++ routes/separarPedido.py
@@ -10,11 +10,30 @@ def buscar_pedido(e, navigate_to, header):
         weight="bold",
         color=colorVariaveis['titulo']
     )
+    inputNumped = ft.TextField(
+        label="Número do pedido",
+        prefix_icon=ft.icons.INSERT_DRIVE_FILE,
+        border_radius=ft.border_radius.all(10),
+        border_color=colorVariaveis['bordarInput'],
+        border_width=2,
+        width=300,
+        keyboard_type=ft.KeyboardType.NUMBER,
+    )
+    buttonBuscarPedido = ft.ElevatedButton(
+        text="Buscar",
+        bgcolor=colorVariaveis['botaoAcao'],
+        color=colorVariaveis['texto'],
+        width=600,
+        on_click=lambda e: print(f"Número do pedido: {inputNumped.value}")
+    )
     return ft.View(
         route="/separar_pedido",
         controls=[
             header,
             title,
+            ft.Container(height=20),
+            inputNumped,
+            buttonBuscarPedido
         ],
         horizontal_alignment=ft.CrossAxisAlignment.CENTER,
     )
