d8e34729608fa732f4a70701d610320fa5da31b4
ThalissonM <manoelthalisson28@gmail.com>
2026-02-02 08:28:13 -0300

    Versão 1.9.0

--- routes/config/config.py
+++ routes/config/config.py
@@ -1,6 +1,6 @@
 import flet as ft
-base_url = "http://192.168.1.99:5000/wmsMobile"
-app_version = '1.8.4'
+base_url = "http://192.168.1.42:5000/wmsMobile"
+app_version = '1.9.0'
 user_info = {}
 colorVariaveis = {
     'botaoAcao': "#0366FF",
